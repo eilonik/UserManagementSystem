76ca87166e21013f943595a37c20d4b5a24b723f
eilonKarty <eilon.karty@mail.huji.ac.il>
2016-12-20 17:12:22 +0200

    recovery mail page built

--- restore.php
+++ restore.php
@@ -7,9 +7,23 @@
             <input type="email" name="email">
             <input type="submit" title="restore">
             <?php
+                include "server.php";
                 if($_SERVER["REQUEST_METHOD"]=="GET"){
                     if($_REQUEST["email"] != ""){
-                        echo "****";
+                        $connection = db_connect();
+                        $email = $_REQUEST["email"];
+                        $query = "SELECT password FROM users WHERE email='$email'";
+                        $result = $connection->query($query);
+                        if($result->num_rows > 0) {
+                            $row = $result->fetch_assoc();
+                            $password = $row["password"];
+                            mail($email,"password recovery",$password);
+                            echo "Recovery mail sent";
+                        }
+                        else{
+                            echo "user does not exist!";
+                        }
+
                     }
                 }
             ?>
